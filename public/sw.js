9678c440eeb1b8b9af1f0bdf0ef29677cacf5b1b
Bakesyo <codymyerslv@gmail.com>
2025-02-07 11:53:13 -0800

    1.2

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'lawsuit-buddy-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/images/hero-bg.webp',
+        '/images/hero-bg-mobile.webp'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
